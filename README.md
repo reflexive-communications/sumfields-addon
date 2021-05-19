e234d571a252f56bfb116473976f7436fd92601c
Sandor Semsey <sandor@es-progress.hu>
2021-05-19 12:44:54 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# sumfields-addon
+
+This extension adds extra summary fields provided by the [Summary Fields](https://github.com/progressivetech/net.ourpowerbase.sumfields)
+extension.
+
+The extension is licensed under [AGPL-3.0](LICENSE.txt).
+
+## Requirements
+
+* PHP v7.3+
+* CiviCRM (5.37 might work below - not tested)
+* net.ourpowerbase.sumfields
+
+## Installation
+
+Sysadmins and developers may clone the [Git](https://en.wikipedia.org/wiki/Git) repo for this extension and
+install it with the command-line tool [cv](https://github.com/civicrm/cv).
+
+```bash
+git clone https://github.com/reflexive-communications/sumfields-addon
+cv en sumfields_addon
+```
+
+## Getting Started
+
+On the **Administer >> Customize Data and Screens >> Summary Fields** admin page, you can find the extra fields.
---
+++ sumfields_addon.php
@@ -0,0 +1,164 @@
+<?php
+
+require_once 'sumfields_addon.civix.php';
+
+// phpcs:disable
+use CRM_SumfieldsAddon_ExtensionUtil as E;
+
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function sumfields_addon_civicrm_config(&$config)
+{
+    _sumfields_addon_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function sumfields_addon_civicrm_xmlMenu(&$files)
+{
+    _sumfields_addon_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function sumfields_addon_civicrm_install()
+{
+    _sumfields_addon_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function sumfields_addon_civicrm_postInstall()
+{
+    _sumfields_addon_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function sumfields_addon_civicrm_uninstall()
+{
+    _sumfields_addon_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function sumfields_addon_civicrm_enable()
+{
+    _sumfields_addon_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function sumfields_addon_civicrm_disable()
+{
+    _sumfields_addon_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function sumfields_addon_civicrm_upgrade($op, CRM_Queue_Queue $queue = null)
+{
+    return _sumfields_addon_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function sumfields_addon_civicrm_managed(&$entities)
+{
+    _sumfields_addon_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
+ */
+function sumfields_addon_civicrm_caseTypes(&$caseTypes)
+{
+    _sumfields_addon_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function sumfields_addon_civicrm_angularModules(&$angularModules)
+{
+    _sumfields_addon_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function sumfields_addon_civicrm_alterSettingsFolders(&$metaDataFolders = null)
+{
+    _sumfields_addon_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function sumfields_addon_civicrm_entityTypes(&$entityTypes)
+{
+    _sumfields_addon_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_themes().
+ */
+function sumfields_addon_civicrm_themes(&$themes)
+{
+    _sumfields_addon_civix_civicrm_themes($themes);
+}
+
+/*
+ * Custom Code
+ */
