499d0f83c7f2b72bade995d4c9926d135c31b4a4
Sandor Semsey <sandor@es-progress.hu>
2021-07-24 22:49:29 +0200

    refactor to Service class

--- sumfields_addon.php
+++ sumfields_addon.php
@@ -168,24 +168,7 @@ function sumfields_addon_civicrm_themes(&$themes)
  */
 function sumfields_addon_civicrm_postJob($job, $params, $result)
 {
-    // Select relevant job
-    if ($job->api_entity !== 'SumFields' || $job->api_action !== 'Gendata') {
-        return;
-    }
-
-    // Parse status
-    $status = $result['values'][0] ?? null;
-    if (preg_match('/New Status: ([a-z]+):([0-9 -:]+)$/', $status, $matches)) {
-        $status_name = $matches[1];
-        $status_date = $matches[2];
-
-        // Check if the last run was successful to avoid loops of failed runs
-        if ($status_name == 'success') {
-            // Change status to scheduled and save so on the next run the data will be regenerated
-            $new_status = 'scheduled:'.$status_date;
-            sumfields_save_setting('generate_schema_and_data', $new_status);
-        }
-    }
+    CRM_SumfieldsAddon_Service::statusToScheduled($job, $result);
 }
 
 /**
